fc88d7c37f5c3486d56257f652e9a8e57d4dd01d
CarlaPerr <Perroudcarla@gmail.com>
2021-10-23 17:23:50 -0300

    commit inicial

---
+++ producto.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+
+#Representa un producto de stock. Tiene un nombre, su marca y precio. 
+
+class Producto: 
+    def __init__(self, nombre, marca, precio):
+        self.nombre = nombre
+        self.marca = marca
+        self.precio = precio 
+
+#Determinar si el producto coincide con el filtro de busqueda.
+#Retornar True si es asi y False de lo contrario.
+#Buscar por el nombre y distinguir mayusculas de minusculas. 
+
+   
